b002e233a8d812f3d0b50812063a7b9f2cdd9eb7
Prus <ada.olga.prus@gmail.com>
2019-07-08 23:44:50 +0200

    Wyświetlenie w mian informacji o ofercie specjalnej

--- src/ShopTest.java
+++ src/ShopTest.java
@@ -9,6 +9,8 @@ public class ShopTest {
         Product product2 = new Product("Smietana", 8.3, "Super gęsta śmietana, idealna do ciast!", category1);
         Product product3 = new Product("Makaron nitki", 4.2, "Makaron idealny do niedzielnego rosołu!", category2);
 
+
+
         System.out.println("Kategoria produktu: " + product1.category.name + ". " + product1.category.description);
         System.out.println(product1.name + ": " + product1.description);
         System.out.println("Cena: " + product1.price + " zł.");
@@ -22,6 +24,15 @@ public class ShopTest {
         System.out.println("Kategoria produktu: " + product3.category.name + ". " + product3.category.description);
         System.out.println(product3.name + ": " + product3.description);
         System.out.println("Cena: " + product3.price + " zł.");
+        System.out.println("\n");
+
+        SpecialOffer specialOffer = new SpecialOffer(product3, "Specjalna okazja w lipcu na makaron nitki!", "07.07.2019 - 31.07.2019", 0.2 );
+        product3.price = product3.price * (1-specialOffer.reduce);
+
+        System.out.println("Oferta specjalna na: " + product3.name);
+        System.out.println(specialOffer.offerDescription);
+        System.out.println("Czas trwania promocji: " + specialOffer.data);
+        System.out.printf("Aktualna cena produktu: %2.2f\n", product3.price);
 
 
 
